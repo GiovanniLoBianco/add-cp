db06facbe555fd81d5378fb527cf66071401b096
giovannilobianco <giovanni.lo-bianco@imt-atlantique.fr>
2020-09-03 22:58:32 +0200

    Initializing project, first commit

---
+++ src/cp/AADDPropagator.java
@@ -0,0 +1,29 @@
+package cp;
+
+import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.exception.ContradictionException;
+import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.util.ESat;
+
+/**
+ * 
+ * Propagator of an AADD (Affine Algebraic Decision Diagram) constraint
+ * 
+ * @author giovannilobianco
+ *
+ */
+public class AADDPropagator extends Propagator<IntVar>{
+
+	@Override
+	public void propagate(int evtmask) throws ContradictionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ESat isEntailed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
